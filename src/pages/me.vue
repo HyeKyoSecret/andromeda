3aea70b69025e0351d0989680db37e8b2e921486
ronnieyuyiwei <ronnieyuyiwei@163.com>
2017-09-28 00:10:50 +0800

    me

--- src/pages/me.vue
+++ src/pages/me.vue
@@ -44,7 +44,15 @@
       </div>
       <div class="foot-menu">
         <div class="button">
-          <img src="">
+          <img src="../img/icon/foot_begin.png">
+          <div>开始</div>
+        </div>
+        <div class="discovery">
+          <img src="../img/icon/foot_discovery.png">
+        </div>
+        <div class="button">
+          <img src="../img/icon/foot_me.png">
+          <div>我</div>
         </div>
       </div>
     </div>
@@ -190,6 +198,39 @@
         }
 
       }
+    }
+    .foot-menu {
+      width: 100%;
+      height: 46px;
+      background: white;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      display: flex;
+    .button{
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+      text-align: center;
+      img {
+        margin: 4px auto 0 auto;
+        width: 21px;
+        height: 21px;
+      }
+      font-size: 12px;
+    }
+    .discovery {
+      flex: 2;
+      img {
+        position: absolute;
+        top: -13px;
+        left: 50%;
+        width: 58px;
+        height: 44px;
+        margin-left: -29px;
+      }
+    }
     }
   }
 </style>

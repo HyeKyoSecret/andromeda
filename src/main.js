15adac80cd8e0ac3ad7d84a3d2da72ba80812088
ronnieyuyiwei <ronnieyuyiwei@163.com>
2017-10-11 20:04:35 +0800

    添加注释

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
-    Axios.get('/checkLogin')
+    Axios.get('/checkLogin')    // 构建登录拦截
       .then((response) => {
         if (!response.data.login) {
           next({
@@ -39,12 +39,12 @@ router.beforeEach((to, from, next) => {
   } else {       // 为不需要登录Auth的页面写入session
     // Axios.get('/checkLogin')
     //   .then((response) => {
-    //     next() // 确保一定要调用 next()
+    //      next()
     //   })
     //   .catch((err) => {
     //     console.log(err)
     //   })
-    next()
+    next() // 确保一定要调用 next()
   }
 })
 /* eslint-disable */

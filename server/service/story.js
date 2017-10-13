77213fd3fb60ccc1b18bbea315cbdc37a34ede67
ronnieyuyiwei <ronnieyuyiwei@163.com>
2017-10-13 19:25:18 +0800

    update dbTest

--- server/service/story.js
+++ server/service/story.js
@@ -40,9 +40,9 @@ router.post('/story/buildStory', (req, res) => {
       console.log(err)
     } else {
       Root.findOne({name: ftNode}) // 判断前趋结点是否为根节点
-        .exec((err, root) => {
-          if (err) {
-            console.log(err)
+        .exec((errRoot, root) => {
+          if (errRoot) {
+            console.log(errRoot)
           } else {
             if (root) {    // 前驱结点是根节点
               console.log('前驱结点是根节点')
@@ -121,10 +121,98 @@ router.post('/story/buildStory', (req, res) => {
                   console.log(err)
                 })
               }
+            } else {      // 前趋结点不是根节点
+              console.log('前驱结点不是根节点')
+              Story.findOne({name: ftNode})
+                .exec((err, odstory) => {
+                  'use strict'
+                  if (err) {
+                    console.log(err)
+                  } else {
+                    if (odstory) {    // 前驱结点是普通故事结点
+                      console.log('前驱结点是普通故事结点')
+                      if (odstory.lc) {   // 普通故事结点lc非空
+                        console.log('普通故事结点lc非空')
+                        console.log('普通故事结点 lc 指向' + odstory.lc)
+                        console.log('开始查找普通故事结点的lc指向的故事')
+                        Story.findOne({_id: odstory.lc})
+                          .exec((err, story) => {
+                            if (err) {
+                              console.log(err)
+                            } else {
+                              if (story) {
+                                console.log('找到故事结点的lc所指向的故事')
+                                let p = story.rb
+                                console.log('p结点赋值，story.rb' + p)
+                                let changeP = function () {
+                                  return new Promise(function (resolve, reject) {
+                                    Story.findOne({_id: p}, (error, nStory) => {
+                                      if (error) {
+                                        console.log(error)
+                                      } else {
+                                        p = nStory.rb
+                                        if (!p) {
+                                          console.log('p已置空')
+                                          nStory.rb = doc._id
+                                          console.log('p的新值' + doc._id)
+                                          nStory.save((err3) => {
+                                            if (err3) {
+                                              console.log(err3)
+                                            } else {
+                                              console.log('写入成功')
+                                            }
+                                          })
+                                        }
+                                        resolve(p)
+                                        reject('error+++++!!')
+                                      }
+                                    })
+                                  })
+                                }
+
+                                let search = async function () {
+                                  while (p) {
+                                    p = await changeP()
+                                    console.log('循环中的p' + p)
+                                  }
+                                  if (!p) {
+                                    console.log('检测到p为空，即表明该节点的右指针为空')
+                                    story.rb = doc._id
+                                    console.log('story.rb' + story.rb)
+                                    story.content = '我要的更新'
+                                    story.save((err4) => {
+                                      if (err4) {
+                                        console.log(err4 + '保存失败')
+                                      }
+                                    })
+                                    console.log(story + '12345689')
+                                  }
+                                }
+
+                                search().catch((err) => {
+                                  'use strict'
+                                  console.log('执行出现了错误' + err)
+                                })
+                              } else {
+                                console.log('普通故事结点的lc故事出错')
+                              }
+                            }
+                          })
+                      } else {
+                        console.log('普通故事结点的lc为空')
+                        odstory.lc = doc._id
+                        odstory.save((err) => {
+                          console.log(err)
+                        })
+                      }
+                    }
+                  }
+                })
             }
           }
         })
     }
   })
 })
+
 module.exports = router

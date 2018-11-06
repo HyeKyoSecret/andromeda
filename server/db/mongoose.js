87054fdb47a1cf7bc400028ce965518cee4af357
ronnieyuyiwei <ronnieyuyiwei@163.com>
2018-11-06 11:02:31 +0800

    11.6

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,11 @@
  */
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/andromeda', {
-  useMongoClient: true
-})
+var uri = 'mongodb://localhost:27017/andromeda,mongodb://localhost:27027/andromeda,mongodb://localhost:27037/andromeda'
+// mongoose.connect('mongodb://localhost:27017', {
+//   useMongoClient: true
+// })
+var opts = {}
+global.db = mongoose.createConnection(uri, opts)
+mongoose.connection = global.db
 module.exports = mongoose
